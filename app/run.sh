aae8026e801c8c01ca1c338cfdbb78916207d832
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-10-27 13:14:39 -0300

    Ajustes performance

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-uvicorn main:app --workers 2 --port $APP_PORT
+uvicorn main:app --workers 1 --port $APP_PORT --backlog 8192  --timeout-keep-alive 1 --no-access-log  #--log-level warning
