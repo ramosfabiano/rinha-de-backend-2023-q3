2e98ee8d08f3ec8d05ec12e6b607b23463e642a3
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-10-28 13:35:50 -0300

    New minimum

--- postgres/config.sql
+++ postgres/config.sql
@@ -2,5 +2,5 @@ ALTER SYSTEM SET listen_addresses = '*' ;
 ALTER SYSTEM SET shared_buffers = '512MB' ;
 ALTER SYSTEM SET max_connections = 400 ;
 ALTER SYSTEM SET log_min_messages = FATAL ;
-ALTER SYSTEM SET effective_io_concurrency = 2 ;
-
+ALTER SYSTEM SET commit_delay = 5;
+ALTER SYSTEM SET commit_siblings = 100;
