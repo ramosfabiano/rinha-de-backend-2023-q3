79ba0439e4e71c989678785a40dcc50bb4ee6225
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-10-30 22:16:32 -0300

    Ajustando ferramenta

--- monitor.py
+++ monitor.py
@@ -13,7 +13,7 @@ def collect_stats() -> dict:
             stats = json.loads(output)
             for s in stats:
                 container_name = s['name']
-                container_stats = {key: s[key] for key in ['cpu_percent', 'mem_percent']  }
+                container_stats = {key: s[key] for key in ['cpu_percent', 'mem_usage']  }
                 if container_name in all_stats:
                     all_stats[container_name].append(container_stats)
                 else:
@@ -30,15 +30,15 @@ def write_stats(stats: dict):
         summary = {}
         for key, entries in stats.items():
             max_cpu_percent = 0.0
-            max_mem_percent = 0.0
+            max_mem_usage = 0.0
             for item in entries:
                 cpu_percent = float(item.get('cpu_percent', '0%').rstrip('%'))
-                mem_percent = float(item.get('mem_percent', '0%').rstrip('%'))
+                mem_percent = float(item.get('mem_usage', '0%').split("MB")[0])
                 max_cpu_percent = max(max_cpu_percent, cpu_percent)
-                max_mem_percent = max(max_mem_percent, mem_percent)
+                max_mem_usage = max(max_mem_usage, mem_percent)
             summary[key] = {
                 'max_cpu_percent': max_cpu_percent,
-                'max_mem_percent': max_mem_percent
+                'max_mem_usage': f'{max_mem_usage}MB'
             }
         print(json.dumps(summary, indent=2))
 
