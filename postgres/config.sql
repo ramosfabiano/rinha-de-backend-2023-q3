3c525d8d49c7f79d762314d8b757a80836a3fbc2
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-10-31 07:44:03 -0300

    Otimizando I/O no  banco

--- postgres/config.sql
+++ postgres/config.sql
@@ -3,3 +3,4 @@ ALTER SYSTEM SET shared_buffers = '512MB' ;
 ALTER SYSTEM SET max_connections = 300 ;
 ALTER SYSTEM SET log_min_messages = FATAL ;
 ALTER SYSTEM SET checkpoint_timeout = 600;
+ALTER SYSTEM SET effective_io_concurrency = 2;
